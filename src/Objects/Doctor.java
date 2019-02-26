bcc08fc3c890433aa0cd1027db6f0c93de838205
Abdullah Al-Wadeiah <abdullah.aw14@gmail.com>
2019-02-26 10:33:36 -0700

    Update Doctor.java

--- src/Objects/Doctor.java
+++ src/Objects/Doctor.java
@@ -10,22 +10,22 @@ public class Doctor
 	//Variables necessary for each doctor
 	private String name;
 	private String department;
-	private String number;
+	private String phoneNumber;
 	private Schedule schedule;
 	
 	//Constructors
 	public Doctor(String name1, String department1, String number1, Schedule schedule1) {
 		setName(name1);
 		setDepartment(department1);
-		setNumber(number1);
+		setPhoneNumber(number1);
 		setSchedule(schedule1);
-		}
+	}
 	public Doctor() { }
 	
 	
 	//setters for Doctor variables
 	public void setName(String name1) { this.name = name1;}
-	public void setNumber(String number1) { this.number = number1;}
+	public void setPhoneNumber(String number1) { this.phoneNumber = number1;}
 	public void setDepartment(String department1) {this.department = department1;}
 	public void setSchedule(Schedule schedule1) {this.schedule = schedule1;}
 	
@@ -33,7 +33,7 @@ public class Doctor
 	
 	//getters for Doctor variables
 	public String getName() {return name;}
-	public String getNumber() {return number;}
+	public String getPhoneNumber() {return phoneNumber;}
 	public String getDepartment() {return department;}
 	public Schedule getSchedule() {return schedule;}
 	
