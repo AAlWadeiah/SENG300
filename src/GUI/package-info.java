20e3b72c529c96a46369c1b13c2509ccf861af4b
alialkh <32747088+alialkh@users.noreply.github.com>
2019-03-03 13:20:51 -0700

    New Package for organizing the code, no more default package

---
+++ src/GUI/package-info.java
@@ -0,0 +1,4 @@
+/** This package will contain all classes involved in making the GUI interface.
+ * 
+ */
+package GUI;
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import GUI.startupGUI;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
