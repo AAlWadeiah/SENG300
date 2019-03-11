f05023ed565eed5696a2d7abf9c19e1de98af66b
AAlWadeiah <abdullah.aw14@gmail.com>
2019-03-11 11:27:28 -0600

    implemented adding, removing, updating, and retrieval of appointments for Schedule

--- src/Objects/Schedule.java
+++ src/Objects/Schedule.java
@@ -2,10 +2,10 @@ package Objects;
 
 import java.util.*;
 
-/** 
- *  
- * This class will be specific to each doctor
- * addAppointment and removeAppointment are not implemented yet
+/**
+ * A class which stores the appointment schedule for a single doctor. The doctor is indicated by their unique ID.
+ * @author Abdullah
+ *
  */
 public class Schedule {
 
@@ -36,14 +36,37 @@ public class Schedule {
 			getCurrentAppointments().put(patientId, newApptSet);
 		}
 	}
-	
+
+	/**
+	 * Retrieves all of the appointments for the given patient, if the patient has any appointments currently booked.
+	 * @param patientId The ID of the patient to grab appointments for
+	 * @return an ArrayList containing all of the patients appointments, and null if no record of this patient could be found
+	 */
+	public ArrayList<Appointment> getAllAppointments(Integer patientId){
+		return getCurrentAppointments().get(patientId);
+	}
+
+	/**
+	 * Retrieves the appointment indicated by an ID for the given patient, if the appointment is found.
+	 * @param patientId The ID of the patient to retrieve the appointment for
+	 * @param appointmentId The ID of the appointment to retrieve
+	 * @return the Appointment for the patient, and null if no Appointment with the ID could be found
+	 */
 	public Appointment getAppointment(Integer patientId, Integer appointmentId) {
 		return getCurrentAppointments().get(patientId).stream().filter(appt -> appointmentId.equals(appt.getAppointmentId())).findFirst().orElse(null);
 	}
-	
-	// TODO: implement retrieval of appointment by date and time
+
+
+	/**
+	 * Retrieves the appointment indicated by a date and time for the given patient, if the appointment is found.
+	 * @param patientId The ID of the patient to retrieve the appointment for
+	 * @param date The date of the appointment to retrieve
+	 * @param time The time of the appointment to retrieve
+	 * @return the Appointment for the patient, and null if no Appointment with the date and time could be found
+	 */
 	public Appointment getAppointment(Integer patientId, String date, String time) {
-		return null;
+		Integer idToFind = Appointment.calculateId(date, time);
+		return getCurrentAppointments().get(patientId).stream().filter(appt -> idToFind.equals(appt.getAppointmentId())).findFirst().orElse(null);
 	}
 
 	/**
@@ -56,27 +79,61 @@ public class Schedule {
 			getCurrentAppointments().remove(patientId);
 			return true;
 		} else {
-			System.out.println("Patient " + patientId + " does not exist");
 			return false;
 		}
 	}
-	
+
 	/**
-	 * 
-	 * @param patientId
-	 * @param appointmentId
-	 * @return
+	 * Removes an appointment specified by the ID for the given patient.
+	 * @param patientId The ID of the patient to remove the appointment for
+	 * @param appointmentId The ID of the appointment to remove
+	 * @return true if the appointment was removed, and false otherwise
 	 */
 	public boolean removeAppointment(Integer patientId, Integer appointmentId) {
-		boolean removed = getCurrentAppointments().get(patientId).removeIf(a -> a.getAppointmentId().equals(appointmentId));
-		return removed;
-	}
-	
-	// TODO: implement removal of appointment by date and time
-	public boolean removeAppointment(Integer patientId, String date, String time) {
-		return false;
+		return getCurrentAppointments().get(patientId).removeIf(a -> a.getAppointmentId().equals(appointmentId));
 	}
 
+	/**
+	 * Removes an appointment specified by a date and time for the given patient.
+	 * @param patientId The ID of the patient to remove the appointment for
+	 * @param date The date of the appointment to remove
+	 * @param time The time of the appointment to remove
+	 * @return true of the appointment was removed, and false otherwise
+	 */
+	public boolean removeAppointment(Integer patientId, String date, String time) {
+		Integer idToFind = Appointment.calculateId(date, time);
+		return getCurrentAppointments().get(patientId).removeIf(a -> a.getAppointmentId().equals(idToFind));
+	}
+
+	/**
+	 * Updates an appointment specified by an ID for a given patient. Sets the date and time of the appointment to the new one's provided.
+	 * @param patientId The ID of the patient to update the appointment for
+	 * @param appointmentId The ID of the appointment to update
+	 * @param newDate The new date for the appointment
+	 * @param newTime The new time for the appointment
+	 * @return the new ID for the appointment
+	 */
+	public Integer updateAppointment(Integer patientId, Integer appointmentId, String newDate, String newTime) {
+		Appointment apptToEdit = getCurrentAppointments().get(patientId).stream()
+				.filter(appt -> appointmentId.equals(appt.getAppointmentId())).findFirst().orElse(null);
+		return apptToEdit.updateAppointment(newDate, newTime);
+	}
+
+	/**
+	 * Updates an appointment specified by the previous date and time for a given patient. Sets the date and time of the appointment to the new one's provided.
+	 * @param patientId The ID of the patient to update the appointment for
+	 * @param oldDate The old date of the appointment
+	 * @param oldTime The old time of the appointment
+	 * @param newDate The new date for the appointment
+	 * @param newTime The new time for the appointment
+	 * @return the new ID for the appointment
+	 */
+	public Integer updateAppointment(Integer patientId, String oldDate, String oldTime, String newDate, String newTime) {
+		Integer idToFind = Appointment.calculateId(oldDate, oldTime);
+		Appointment apptToEdit = getCurrentAppointments().get(patientId).stream()
+				.filter(appt -> idToFind.equals(appt.getAppointmentId())).findFirst().orElse(null);
+		return apptToEdit.updateAppointment(newDate, newTime);
+	}
 
 	//Setters
 	public void setDoctorId(Integer doctorId) {this.doctorId = doctorId;}
