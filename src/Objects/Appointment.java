9e7e73342276fa519c075970639d7d36693b4d63
AAlWadeiah <abdullah.aw14@gmail.com>
2019-03-10 23:42:55 -0600

    added unique ID field and some functionality for Appointment, redesigned Schedule to store and work with appointments and patients

--- src/Objects/Appointment.java
+++ src/Objects/Appointment.java
@@ -1,25 +1,44 @@
 package Objects;
 
 public class Appointment {
+	private Integer appointmentId;
 	private Integer patientId;
 	private String date;
 	private String time;
 	
+	// Constructors
 	public Appointment() {}
 	public Appointment(Integer pId, String date, String time) {
 		setPatientId(pId);
 		setDate(date);
 		setTime(time);
+		setAppointmentId(date.hashCode() + time.hashCode() + pId);
+	}
+	
+	/**
+	 * Updates the appointment by changing the date and time to the new one's provided.
+	 * @param newDate The new date for the appointment
+	 * @param newTime The new time for the appointment
+	 * @return the new ID for the appointment
+	 */
+	public Integer updateAppointment(String newDate, String newTime) {
+		setDate(newDate);
+		setTime(newTime);
+		Integer newAppointmentId = newDate.hashCode() + newTime.hashCode() + getPatientId();
+		setAppointmentId(newAppointmentId);
+		return newAppointmentId;
 	}
 	
 	// Getters
 	public Integer getPatientId() {return patientId;}
 	public String getTime() {return time;}
 	public String getDate() {return date;}
+	public Integer getAppointmentId() {return appointmentId;}
 	
 	// Setters
 	public void setPatientId(Integer patientId) {this.patientId = patientId;}
 	public void setTime(String time) {this.time = time;}
 	public void setDate(String date) {this.date = date;}
+	private void setAppointmentId(Integer appointmentId) {this.appointmentId = appointmentId;}
 	
 }
--- src/Objects/Schedule.java
+++ src/Objects/Schedule.java
@@ -1,37 +1,80 @@
 package Objects;
+
+import java.util.*;
+
 /** 
  *  
  * This class will be specific to each doctor
  * addAppointment and removeAppointment are not implemented yet
  */
 public class Schedule {
-	
+
 	//Fields
-	private Doctor doctor;
-	
-	
+	private Integer doctorId;
+	private HashMap<Integer, ArrayList<Appointment>> currentAppointments;
+
 	//Constructors 
 	public Schedule () {}
-	public Schedule (Doctor doctor1) {setDoctor(doctor1);}
-	
-	
-	//Unimplemented class for creating Schedule
-	public void createSchedule() {}
-	
-	
-	//Appointment Methods
-	public void addAppointment(String time, Patient patient)
-	{
-		if (patient.getDoctor().equals(doctor.getName())) {}	//patient class only stores doctor Name
-	}															//so we compare it to the name of the doctor who holds this schedule
-	public void removeAppointment(String time, Patient patient)
-	{
-		if (patient.getDoctor().equals(doctor.getName())) {}
+	public Schedule (Integer doctorId) {
+		setDoctorId(doctorId);
+		setCurrentAppointments(new HashMap<Integer, ArrayList<Appointment>>());
+	}	
+
+	/**
+	 * Creates a new appointment for the specified patient who is indicated by the given ID.
+	 * If no appointments have been previously made for this patient, then a new entry in the currentAppointments HashMap will be created for the patient.
+	 * @param patientId The ID of the patient to make the appointment for.
+	 * @param date The date of the appointment
+	 * @param time The time of the appointment
+	 */
+	public void addAppointment(Integer patientId, String date, String time) {
+		if (getCurrentAppointments().containsKey(patientId)) {
+			getCurrentAppointments().get(patientId).add(new Appointment(patientId, date, time));
+		} else {
+			ArrayList<Appointment> newApptSet = new ArrayList<>();
+			newApptSet.add(new Appointment(patientId, date, time));
+			getCurrentAppointments().put(patientId, newApptSet);
+		}
 	}
 	
+	public Appointment getAppointment(Integer patientId, Integer appointmentId) {
+		return getCurrentAppointments().get(patientId).stream().filter(appt -> appointmentId.equals(appt.getAppointmentId())).findFirst().orElse(null);
+	}
 	
-	//Setter and getter for doctor
-	public void setDoctor(Doctor doctor1) {}
-	public Doctor getDoctor() {return doctor;}
+	// TODO: implement retrieval of appointment by date and time
+	public Appointment getAppointment(Integer patientId, String date, String time) {
+		return null;
+	}
+
+	/**
+	 * Removes the entry of the given patient from the currentApointments HashMap.
+	 * @param patientId The ID of the patient to remove from the appointments record.
+	 */
+	public void removePatient(Integer patientId) {
+		if (getCurrentAppointments().containsKey(patientId)) {
+			getCurrentAppointments().remove(patientId);
+		} else {
+			System.out.println("Patient " + patientId + " does not exist");
+		}
+	}
+	
+	public boolean removeAppointment(Integer patientId, Integer appointmentId) {
+		boolean removed = getCurrentAppointments().get(patientId).removeIf(a -> a.getAppointmentId().equals(appointmentId));
+		return removed;
+	}
+	
+	// TODO: implement removal of appointment by date and time
+	public boolean removeAppointment(Integer patientId, String date, String time) {
+		return false;
+	}
+
+
+	//Setters
+	public void setDoctorId(Integer doctorId) {this.doctorId = doctorId;}
+	public void setCurrentAppointments(HashMap<Integer, ArrayList<Appointment>> currAppt) {this.currentAppointments = currAppt;}
+
+	// Getters
+	public Integer getDoctor() {return doctorId;}
+	public HashMap<Integer, ArrayList<Appointment>> getCurrentAppointments() {return currentAppointments;}
 
 }
