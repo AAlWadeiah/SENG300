977828e5cc0159e2808598148379104d9ff3e497
Ivan Ristanen <ilristan@ucalgary.ca>
2019-03-13 23:10:22 -0600

    Progress made on login feature

--- src/GUI/validateAccount.java
+++ src/GUI/validateAccount.java
@@ -1,6 +1,10 @@
 package GUI;
 
+import JsonFileUtils.Parser;
+import Objects.Patient;
+
 import java.io.File;
+import java.util.List;
 
 /**
  * Ivans stuff will be in this method or something similar
@@ -9,9 +13,35 @@ public class validateAccount {
 
 	public validateAccount(String user, String pass, String filename) {this.validate(user, pass, filename);}
 	public validateAccount() {}
-	
+
+
+
+	String currentDir = System.getProperty("user.dir");
+	File path = new File(currentDir);
+	Parser parser = new Parser();
+	File[] jsonFiles = parser.getFiles(path);
+	List<Patient> allPatients = parser.parsePatients(jsonFiles);
+
+
+
+
 	public Boolean validate(String user, String pass, String filename) {
-		if (filename.equals("Patient_Users.json" )) { System.out.println("This is a patient signing in:\n" + user +" + "+pass);return true;}
+
+		if (filename.equals("Patient_Users.json" )) {
+			for (Patient patient : allPatients) {
+				//System.out.println(patient.getFirstName() + " " + patient.getLastName()+" " + patient.getAddress() +" " + patient.getDoctor() + " " + patient.getEmail() + " " +patient.getNumber() + " " + patient.getId());
+				if (patient.getFirstName().equals(user)) {
+
+					System.out.println("Found User!");
+				}
+				else{
+
+					System.out.println("User Doesn't Exits!");
+				}
+			}
+
+			System.out.println("This is a patient signing in:\n" + user +" + "+pass);return true;
+		}
 		else if (filename.equals("Doctor_Users.json")) {System.out.println("this is a doctor signing in\n"+ user +" + "+pass); return true;}
 		else {System.out.println("This is an admin signing in"+ user +" + "+ pass); return true;}
 }}
