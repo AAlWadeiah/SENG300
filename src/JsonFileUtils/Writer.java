d0fc55bc1a94bf39935b6bf890a5f5f1f222fb15
falu85 <fahreen.chernezky@ucalgary.ca>
2019-03-17 15:16:56 -0600

    All new Doctors are saved to one file

--- src/JsonFileUtils/Writer.java
+++ src/JsonFileUtils/Writer.java
@@ -3,11 +3,8 @@ package JsonFileUtils;
 import java.io.*;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 
-import Objects.Schedule;
 import com.google.gson.Gson;
 
 import Objects.Patient;
@@ -44,7 +41,7 @@ public class Writer {
 
 		}
 		catch (IOException e){
-			System.out.println("Error! Cannot close file - Big trouble...Aborting");
+			System.out.println("Error! Writer Class cannot close allThePatients file - Big trouble...Aborting");
 			return false;
 		}
 
@@ -61,20 +58,56 @@ public class Writer {
 			file.write(newJsonString);
 		}
 		catch(IOException f){
-			System.out.println("failed to create Patient JSON object file");
+			System.out.println("Error! Writer Class failed to create Patient JSON object file");
 			return false;
 		}
 		return true;
 	}
 	public boolean writeObjectToFile(Doctor doc){
-		String jsonString = new Gson().toJson(doc);
-		try (FileWriter file = new FileWriter(doc.getDepartment()+"_"+doc.getFirstname()+".json")){
-			file.write(jsonString);
+
+
+		//Note: source for how to append code was:
+		//https://stackoverflow.com/questions/54574576/append-json-object-in-existing-json-file-using-java
+		//read old file, rewrite to new file with new information to be appended
+		Gson allDocFile = new GsonBuilder().setPrettyPrinting().create();
+		Type doctorType = new TypeToken<List<Doctor>>(){}.getType();
+
+		//declared addNewPatient
+		List<Doctor> addNewDoctor;
+
+
+		try {
+			FileReader fReader = new FileReader("allTheDoctors.json");
+			addNewDoctor = allDocFile.fromJson(fReader, doctorType);
+			fReader.close();
+		}
+		catch(FileNotFoundException e){
+			addNewDoctor = null;
+
+		}
+		catch (IOException e){
+			System.out.println("Error! Writer Class cannot close allTheDoctors file - Big trouble...Aborting");
+			return false;
+		}
+
+		//if file is empty create a new list
+		if (null == addNewDoctor){
+			addNewDoctor = new ArrayList<>();
+		}
+		//to append, rewrite to new file
+		addNewDoctor.add(doc);
+		String newJsonString = allDocFile.toJson(addNewDoctor);
+
+
+		try (FileWriter file = new FileWriter("allTheDoctors.json")){
+			file.write(newJsonString);
 		}
 		catch(IOException f){
-			System.out.println("failed to create Doctor JSON object file");
+			System.out.println("Error! Writer class failed to create Doctor JSON object file");
 			return false;
 		}
 		return true;
+
+
 	}
 }
