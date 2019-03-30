922383a3ec50a1efa12751f36cb80b5c1bab44d3
Ivan Ristanen <ilristan@ucalgary.ca>
2019-03-29 18:10:50 -0600

    Removed "name" from admin
    This is redundant

--- src/Objects/Admin.java
+++ src/Objects/Admin.java
@@ -1,33 +1,26 @@
 package Objects;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
 /** This method is used to handle administrators in the system
  * 
  *
  */
 public class Admin {
 
-	private String name;
 	private Integer id;
 	private String password = "5f4dcc3b5aa765d61d8327deb882cf99"; // "password"
 	
 	//Constructors
-	public Admin (Integer id, String name) { 
+	public Admin (Integer id) { 
 		setId(id);
-		setName(name);
 	}
 	public Admin() {
-		this(null, null);
+		this(null);
 	}
 	
 	//Setters
-	public void setName(String name1) {this.name = name1;}
 	public void setId(Integer id) {this.id = id;}
 		
 	//Getters
-	public String getName() {return name;}
 	public Integer getId() {return id;}
 	public String getPassword() {return password;}
 }
